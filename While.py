dd93a5a40710b7a63483191dd99d7fc909307edc
lenlalo <leonardo.novelli@uner.edu.ar>
2025-05-03 14:05:50 -0300

    3_5_2025_tiempo y sonido

---
+++ While.py
@@ -0,0 +1,17 @@
+a=int(input("Ingrese un numero: "))
+# while a<10:
+#     a=a+1
+#     print (a)
+# print("Finalizo el ciclo while")
+
+
+while a<10:
+    for i in range(1,11,2):
+        a=a+i #a la variable a se le suma i. También se puede poner como a+=i
+        print(a)
+print("Fin del bucle")
+
+        
+        
+        
+
